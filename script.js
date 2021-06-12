46c5f06ec179810dd4215a4d44a9353bd854f482
Kasper Nikolajsen Dahl <kasp103y@stud.kea.dk>
2021-06-12 14:39:33 +0200

    udkommenteret sidenVises function

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const navMenu = document.querySelector(".nav_menu");
 const burger = document.querySelector(".hamburger");
 
-window.addEventListener("load", sidenVises);
-function sidenVises() {
-  console.log("SidenVises");
-}
+//window.addEventListener("load", sidenVises);
+//function sidenVises() {
+//  console.log("SidenVises");
+//}
 
 burger.addEventListener("click", mobil);
 
